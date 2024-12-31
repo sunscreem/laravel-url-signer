844da039bb9adca4c7911193574d773c72670243
Robert Cooper <rob.cooper@sellonlinedirect.com>
2024-12-31 18:49:31 +0000

    Update UrlSigner.php

--- src/UrlSigner.php
+++ src/UrlSigner.php
@@ -3,14 +3,14 @@
 namespace Spatie\UrlSigner\Laravel;
 
 use DateTime;
-use Spatie\UrlSigner\Md5UrlSigner;
+use Spatie\UrlSigner\Sha256UrlSigner;
 
-class UrlSigner extends Md5UrlSigner
+class UrlSigner extends Sha256UrlSigner
 {
     public function sign(
         string $url,
-        int|DateTime|null $expiration = null,
-        ?string $signatureKey = null,
+        $expiration = null,
+        $signatureKey = null,
     ): string {
         $expiration ??= config('url-signer.default_expiration_time_in_seconds');
 
